afa79b8d275ac696ee6d4125b598b86cd7b168ed
audilin0407-commits <audilin0407@gmail.com>
2025-12-13 17:59:31 +0800

    Week2 practice uploaded
    Add a script to find multiples of 3 and count words

---
+++ week2/week2_practice.py
@@ -0,0 +1,18 @@
+result = []
+
+for i in range(1, 101):
+    # 在這裡加入你的條件
+    if i % 3 == 0:
+        result.append(i)
+    pass
+
+print(result)
+
+
+
+s = "I love learning data science with AI"
+new_s = s.split()
+num = 0
+for n in new_s:
+    num += 1
+print(num)
